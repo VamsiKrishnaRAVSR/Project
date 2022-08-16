7cbe51e49a5968cc5964bb3c94e767067c189ef5
VamsiKrishnaRAVSR <vamsi.krishna@joshsoftware.com>
2022-08-16 12:34:23 +0530

    features issues

--- src/hooks/getPatchTodo.tsx
+++ src/hooks/getPatchTodo.tsx
@@ -1,10 +1,12 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { queryConstants } from "../constants/queryConstants";
 import { patchTodo } from "../services/todos.services";
 import { Todo } from "../types";
 
 const useGetPatchTodo = () => {
+  const queryClient = useQueryClient();
   return useMutation((props: Todo) => patchTodo(props), {
-    
+    onSuccess: () => queryClient.invalidateQueries(queryConstants.GET_TODO),
     onError: (err) => console.log(err),
   });
 };
