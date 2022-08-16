5d17b5cdcf39acdfd28e82810e4508c44d778f82
VamsiKrishnaRAVSR <vamsi.krishna@joshsoftware.com>
2022-08-16 12:27:30 +0530

    Types defined

--- src/hooks/getTodo.hooks.tsx
+++ src/hooks/getTodo.hooks.tsx
@@ -1,10 +1,10 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 import { queryConstants } from "../constants/queryConstants";
 import { getTodo } from "../services/todos.services";
 import { Todo } from "../types";
 
 const useGetTodo = (props: string) => {
-  const queryClient = useQueryClient();
+  const queryClient: QueryClient = useQueryClient();
 
   return useQuery<Todo>([queryConstants.GET_TODO], () => getTodo(props), {
     onError: (err) => {
@@ -13,8 +13,7 @@ const useGetTodo = (props: string) => {
     initialData: () => {
       const p = queryClient
         .getQueriesData(queryConstants.ALL_TODOS)[0][1]
-        .find((ele) => ele.id === parseInt(props));
-
+        .find((ele: Todo) => ele.id === parseInt(props));
       console.log(p);
       if (p) {
         return p;
